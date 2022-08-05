0337108a8b542f7db279ce60341aa42f0c1219ae
btnguyenPersonal <btnguyenPersonal@github..com>
2022-08-05 18:53:27 -0500

    added printing for screen and changing screen

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,6 @@ mod screen;
 
 fn main() {
     let mut code_screen = screen::Screen::new(80, 24);
+    screen::Screen::change(&mut code_screen, 50, 5, 'h');
+    screen::Screen::display(&mut code_screen);
 }
--- src/screen.rs
+++ src/screen.rs
@@ -1,15 +1,22 @@
 pub struct Screen {
-    row: usize,
-    col: usize,
     data: Vec<Vec<char>>
 }
 
 impl Screen {
-    pub fn new(r: usize, c: usize) -> Self {
+    pub fn new(c: usize, r: usize) -> Self {
         Screen {
-            row: r,
-            col: c,
-            data: vec![vec!['#'; c]; r]
+            data: vec![vec![' '; c]; r]
         }
+    }
+    pub fn display(&mut self) {
+        for r in &self.data {
+            for c in r {
+                print!("{}", c.to_string());
+            }
+            println!();
+        }
+    }
+    pub fn change(&mut self, c: usize, r: usize, newval: char) {
+        self.data[r][c] = newval;
     }
 }
