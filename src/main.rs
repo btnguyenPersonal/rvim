de92adf95bab2a7d25b45c36069f73e3cc74d961
btnguyenPersonal <btnguyenPersonal@github..com>
2022-08-05 20:02:14 -0500

    added tests

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ mod screen;
 
 fn main() {
     let mut code_screen = screen::Screen::new(80, 24);
-    screen::Screen::change(&mut code_screen, 50, 5, 'h');
-    screen::Screen::display(&mut code_screen);
+    screen::Screen::set(&mut code_screen, 50, 5, 'h');
+    println!("{}", screen::Screen::display(&mut code_screen));
     println!("{}", screen::Screen::get(code_screen, 4, 5));
 }
--- src/screen.rs
+++ src/screen.rs
@@ -5,21 +5,118 @@ pub struct Screen {
 impl Screen {
     pub fn new(c: usize, r: usize) -> Self {
         Screen {
-            data: vec![vec!['.'; c]; r]
+            data: vec![vec![' '; c]; r]
         }
     }
-    pub fn display(&mut self) {
+    pub fn display(&mut self) -> String{
+        let mut output = "".to_string();
         for r in &self.data {
             for c in r {
-                print!("{}", c.to_string());
+                output += &c.to_string();
             }
-            println!();
+            output += "\n";
         }
+        output
     }
-    pub fn change(&mut self, c: usize, r: usize, newval: char) {
+    pub fn set(&mut self, c: usize, r: usize, newval: char) {
+        if c >= self.data[r].len() || r >= self.data.len() {
+            panic!("Tried to set element with bigger index than screen length");
+        }
         self.data[r][c] = newval;
     }
     pub fn get(self, c: usize, r: usize) -> char {
+        if c >= self.data[r].len() || r >= self.data.len() {
+            panic!("Tried to get element with bigger index than screen length");
+        }
         self.data[r][c]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_creates_0_length_screen() {
+        let test_screen = Screen::new(0, 0);
+        assert_eq!(test_screen.data.len(), 0);
+    }
+
+    #[test]
+    fn new_creates_correct_dimensions() {
+        let test_screen = Screen::new(5, 4);
+        assert_eq!(test_screen.data[0].len(), 5);
+        assert_eq!(test_screen.data.len(), 4);
+    }
+
+    #[test]
+    fn new_creates_80_24_screen() {
+        let test_screen = Screen::new(80, 24);
+        assert_eq!(test_screen.data[0].len(), 80);
+        assert_eq!(test_screen.data.len(), 24);
+    }
+
+    #[test]
+    fn set_default_case() {
+        let mut test_screen = Screen::new(80, 24);
+        Screen::set(&mut test_screen, 10, 10, '$');
+        assert_eq!(test_screen.data[10][10], '$')
+    }
+
+    #[test]
+    fn display_default_case() {
+        let mut test_screen = Screen::new(80, 24);
+        let mut correct_output = "".to_string();
+        for _i in 0..24 {
+            for _j in 0..80 {
+                correct_output += &" ".to_string();
+            }
+            correct_output += "\n";
+        }
+        assert_eq!(Screen::display(&mut test_screen), correct_output)
+    }
+
+    #[test]
+    fn display_0_by_0_case() {
+        let mut test_screen = Screen::new(0, 0);
+        assert_eq!(Screen::display(&mut test_screen), "")
+    }
+
+    #[test]
+    fn display_after_set_case() {
+        let mut test_screen = Screen::new(4, 4);
+        Screen::set(&mut test_screen, 0, 0, '$');
+        Screen::set(&mut test_screen, 1, 1, '$');
+        Screen::set(&mut test_screen, 2, 2, '$');
+        Screen::set(&mut test_screen, 3, 3, '$');
+        let correct_output = "$   \n $  \n  $ \n   $\n";
+        assert_eq!(Screen::display(&mut test_screen), correct_output)
+    }
+
+    #[test]
+    #[should_panic]
+    fn set_panic_larger_col() {
+        let mut test_screen = Screen::new(80, 24);
+        Screen::set(&mut test_screen, 10, 24, '$');
+    }
+
+    #[test]
+    #[should_panic]
+    fn set_panic_larger_row() {
+        let mut test_screen = Screen::new(80, 24);
+        Screen::set(&mut test_screen, 80, 10, '$');
+    }
+
+    #[test]
+    fn get_returns_correct_default_val() {
+        let test_screen = Screen::new(80, 24);
+        assert_eq!(Screen::get(test_screen, 10, 10), ' ');
+    }
+
+    #[test]
+    fn get_returns_correct_val_after_set() {
+        let mut test_screen = Screen::new(80, 24);
+        Screen::set(&mut test_screen, 10, 10, '*');
+        assert_eq!(Screen::get(test_screen, 10, 10), '*');
     }
 }
